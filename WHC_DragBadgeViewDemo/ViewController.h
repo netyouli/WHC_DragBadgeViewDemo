057087100e81c11200dbeb2cfd208dceed1853b5
netyouli <712641411@qq.com>
2015-06-15 15:06:39 +0800

    Initial Commit

---
+++ WHC_DragBadgeViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WHC_DragBadgeViewDemo
+//
+//  Created by 吴海超 on 15/6/15.
+//  Copyright (c) 2015年 吴海超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
